a6b1d246a6fbf5c98d2ce10b7f2297f96c4acacc
patriciaaguayo <patriciaaguayop@gmail.com>
2025-02-25 18:50:30 +0100

    Hecho programa de Filosofos.

---
+++ Filosofos/src/Filosofo.java
@@ -0,0 +1,48 @@
+import java.util.concurrent.Semaphore;
+
+class Filosofo implements Runnable {
+    private final int id;
+    private final Semaphore tenedorIzquierdo;
+    private final Semaphore tenedorDerecho;
+    private final Semaphore mayordomo;
+
+    public Filosofo(int id, Semaphore tenedorIzquierdo, Semaphore tenedorDerecho, Semaphore mayordomo) {
+        this.id = id;
+        this.tenedorIzquierdo = tenedorIzquierdo;
+        this.tenedorDerecho = tenedorDerecho;
+        this.mayordomo = mayordomo;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                pensar();
+                mayordomo.acquire(); // Solicita permiso al mayordomo para comer
+                tenedorIzquierdo.acquire();
+                tenedorDerecho.acquire();
+                comer();
+                tenedorDerecho.release();
+                tenedorIzquierdo.release();
+                mayordomo.release(); // Libera el permiso del mayordomo para que otros filósofos puedan comer
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    // Método para simular la acción de pensar
+
+    private void pensar() throws InterruptedException {
+        System.out.println("\n El filósofo " + (id+1) + " está pensando.");
+        Thread.sleep((long) (Math.random() * 1000));
+    }
+
+    // Método para simular la acción de comer
+
+    private void comer() throws InterruptedException {
+        System.out.println("\n Filósofo " + (id+1) + " comiendo.");
+        Thread.sleep((long) (Math.random() * 1000));
+    }
+}
+
---
+++ Filosofos/src/Filosofos.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.Semaphore;
+
+public class Filosofos {
+    private static final int NUM_FILOSOFOS = 5;// Permite solo 5 filósofos
+    private static final Filosofo[] filosofos = new Filosofo[NUM_FILOSOFOS]; // Permite solo 1 filósofo pensar
+    private static final Semaphore[] tenedores = new Semaphore[NUM_FILOSOFOS]; // Permite solo 1 filósofo comer
+    private static final Semaphore mayordomo = new Semaphore(NUM_FILOSOFOS - 1); // Permite solo 4 filósofos comiendo
+
+    public static void main(String[] args) {
+        for (int i = 0; i < NUM_FILOSOFOS; i++) {
+            tenedores[i] = new Semaphore(1); // Permite solo 1 filósofo comer
+        }
+        for (int i = 0; i < NUM_FILOSOFOS; i++) {
+            filosofos[i] = new Filosofo(i, tenedores[i], tenedores[(i + 1) % NUM_FILOSOFOS], mayordomo);
+            new Thread(filosofos[i]).start();
+        }
+    }
+}
